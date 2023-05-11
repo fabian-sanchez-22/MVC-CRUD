d6e20b2a8300d9813f7b2fc9ca4eb57b6fe99c5d
fabian-sanchez-22 <fsanchez187@misena.edu.co>
2023-05-10 19:12:14 -0500

    Primer Commit MVC

---
+++ view/roles.frm.php
@@ -0,0 +1,23 @@
+<?php include_once "header.php"; ?>
+<div class="row my-5">
+    <h1 class="text-center  ">FORMULARIO ROLES</h1>
+</div>
+
+<div class="row">
+    <div class="col-3 d-flex justify-content-end align-items-center">
+        <span class="fw-bolder" >Nombre Rol:</span>
+    </div>
+
+    <div class="col-7">
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" placeholder="admin">
+            <label for="floatingInput">Nombre rol</label>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-5 bg-dark">
+    <h1 class="text-white text-center" >Tabla de roles</h1>
+</div>
+
+<?php include_once "footer.php"; ?>
